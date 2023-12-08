12d0639051350df20d8785dc29144c33528d2eb0
Tachy-22 <entekumejeffrey@gmail.com>
2023-12-08 14:11:16 +0100

    commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,12 +5,14 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <section className={` w-full h-[90dvh] bg-cover overflow-hidden relative `}>
+    <section
+      className={` w-full min-h-[90dvh] h-[90vh]  bg-cover overflow-hidden relative `}
+    >
       {/* <LightOrbs position="top" /> */}
       <div className=" md:p-[2rem] sm:p-[1rem] p-4 pb-0  h-full uppercase w-full flex flex-col justify-between xl:items-center">
         <div className="flex flex-col justify-center items-center  h-full">
           {" "}
-          <h1 className="  font-extrabold xl:text-9xl h-fit sm:text-7xl text-3xl tracking-wide monoton lg:text-center text-start">
+          <h1 className="  font-extrabold xl:text-9xl h-fit sm:text-7xl text-3xl tracking-wide monoton lg:text-center text-start  w-full ">
             Jeffery Entekume
           </h1>
           <p className="xl:text-6xl sm:text-5xl h-fit text-2xl lg:text-center text-start  text-white/70 font-extralight tracking-widest lg:pb-[8rem] pb-[3rem]">
